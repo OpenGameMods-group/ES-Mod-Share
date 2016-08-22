06c806a1e779b93d1daaf6fc51ae8145ad520f11
Evan <evan4153@gmail.com>
2016-08-21 18:38:15 -0700

    HTTPS Fix and Default Image
    Links should be HTTPS or inside the img folder.
    If an image fails to load a default one will be used.

--- js/mods.js
+++ js/mods.js
@@ -11,11 +11,15 @@ function createMod(title, author, description, image, download, infoLink, tags) 
 	return mod;
 }
 
+// GUIDELINES
 // Add mods to this list
 // Type createMod(),
 // Then between the parenthesis in quotes type your mod in this order: title, author, description, image, download, infoLink, tags
 // Download, and image should be a link
 // Put new mods at the bottom.
+// USE HTTPS for external links or add your image to the img folder and link to that.
+// Don't copy steam preview image urls upload to imgur or to the repo
+// Use unique names
 
 // F.A.Q
 // No info link? Just put a #
@@ -32,7 +36,7 @@ createMod(
 	"Example Author",
 	// Use single quotes if you have double quotes inside your text like this
 	'This is just an "example" inside the mods file so people can see how to add their own mod',
-	"http://images.akamai.steamusercontent.com/ugc/278478748956617574/8C2F832290DBE94C3EFF2FF6FB1068C8589BEA60/",
+	"img/mods/example.jpg",
 	"https://www.youtube.com/watch?v=32UGD0fV45g",
 	"https://www.google.com/",
 	"example"
@@ -42,9 +46,9 @@ createMod(
 	"Businessman",
 	"Legolas",
 	"Become a ruler of the galaxy through money. Buy trade routes, stocks, set up shops and entertainment centers, buy planets, hire a mercenary army, hunt down people who didn't pay back their loans....and more..Also includes an optional alternate pirate start, and additons to make piracy much more viable and fun.",
-	"img/businessman.png",
+	"img/mods/businessman.png",
 	"https://drive.google.com/open?id=0B9ljHWQDV1lIa3h0NFAyV3B3M2M",
-	"http://steamcommunity.com/sharedfiles/filedetails/?id=690480787",
+	"https://steamcommunity.com/sharedfiles/filedetails/?id=690480787",
 	"business, economy, strategy, merchant"
 	),
 
@@ -54,7 +58,7 @@ createMod(
 	"Enigma Corp is an ever expanding mod that takes you through the adventures of the Enigma Corporation, a strange and greedy corporation that has only just recently surfaced onto the open market and is already doing big things. Both an outfit development and ship creation company, the Corporation has taken the market by storm, introducing new and exciting things to the general public that have never been seen before.",
 	"https://avatars0.githubusercontent.com/u/11450262?v=3&s=400",
 	"https://github.com/Amazinite/Enigma-Corp/releases",
-	"http://steamcommunity.com/app/404410/tradingforum/365172547939442297/",
+	"https://steamcommunity.com/app/404410/tradingforum/365172547939442297/",
 	"wip, rel, sexy"
 	),
 
@@ -62,9 +66,9 @@ createMod(
 	"Ship Generator",
 	"Legolas",
 	"Ever wanted to easily create ships? (as if it's not already easy) Well, now you can! I created a simple tool that will generate the code for a ship.",
-	"http://images.akamai.steamusercontent.com/ugc/259337442164610302/4B72FEBE60A2E1A95D7F9A3422EE4179270D0181/",
+	"img/mods/shipgen82116.jpg",
 	"https://evan153.github.io/Endless-Sky-Ship-Generator/",
-	"http://steamcommunity.com/sharedfiles/filedetails/?id=718279752",
+	"https://steamcommunity.com/sharedfiles/filedetails/?id=718279752",
 	"tool, ship, code, modding"
 	),
 
@@ -72,7 +76,7 @@ createMod(
 	"S-Kestrel Super",
 	"mdsmestad",
 	"The S-Kestrel Super is a cheaty ship, and more of a work of art and testing tool than a ship that's actually meant for the game. I use it in mod for testing mostly, sometimes I Just look at it for a while and admire it. Please enjoy hours of make little pirates go BOOM! ",
-	"http://i.imgur.com/VglrdGr.jpg",
+	"https://i.imgur.com/VglrdGr.jpg",
 	"https://drive.google.com/open?id=0BwfUPLPyuB65U1poRW5RcGgySVU",
 	"#",
 	"cheat, ship, testing, art"
@@ -82,8 +86,8 @@ createMod(
 	"Ship Builder",
 	"Endless Sky",
 	"Get position coordinates for your ships.",
-	"http://i.imgur.com/rHP0mXz.jpg",
-	"http://endless-sky.github.io/ship_builder.html",
+	"https://i.imgur.com/rHP0mXz.jpg",
+	"https://endless-sky.github.io/ship_builder.html",
 	"https://github.com/endless-sky/endless-sky/wiki/CreatingShips",
 	"ship, modding, tool"
 	),
