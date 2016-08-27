c3ca7c2e2115b0c34d7d502cf464d35404fba5ae
Storm Crow <teridaxofmetrunui@gmail.com>
2016-08-27 19:37:00 +0100

    Update mods.js

--- js/mods.js
+++ js/mods.js
@@ -116,7 +116,6 @@ createMod(
 createMod(
 	"Le Troll",
 	"Storm Crow (Inspired by Derpy Horse)",
-	// Use single quotes if you have double quotes inside your text like this
 	"A Star Barge called "Le Troll" that nukes you on sight no matter what is going on. In the middle of the campaign? Nuked. Just got your first Kor ship? Nuked. Kappa.",
 	"https://raw.githubusercontent.com/endless-sky/endless-sky-high-dpi/master/images/ship/star%20barge%402x.png",
 	"https://drive.google.com/file/d/0B-tyb7FBVShNV3hPQ042R3hsNXc/view?usp=sharing",
